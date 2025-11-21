e7a69a01bbee5edcb7011844cda8b64f1adcced7
korobe0906 <laptrinh0906@gmail.com>
2025-11-22 03:15:49 +0700

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(3001);
   console.log('BrainBattle Duo service is running on http://localhost:3001/api');
 }
